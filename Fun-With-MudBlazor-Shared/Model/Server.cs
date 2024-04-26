68e75fa4ad14007b98d38ed98b3a37ff2852a7f2
Rick Miskin II <richard.miskin@inmar.com>
2024-04-26 12:41:40 -0400

    Added advanced delete popup

---
+++ Fun-With-MudBlazor-Shared/Model/Server.cs
@@ -0,0 +1,10 @@
+﻿namespace Blazor_Mud_Shared.Model
+{
+    public class Server
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string IpAddress { get; set; }
+    }
+}
